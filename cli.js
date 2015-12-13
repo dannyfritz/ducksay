6603fb9aaf2b13ec0601d2644fc5b837f420eded
Danny Fritz <dannyfritz@gmail.com>
2015-12-12 19:44:08 -0700

    :sparkles: Accept stdin input. Fixes #1

--- cli.js
+++ cli.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 'use strict'
 
+var getStdin = require('get-stdin')
 var ducksay = require('../ducksay')
+var trim = require('trim')
 
-var argv = require('yargs').argv;
-
-console.log(ducksay(argv._.join(' ')))
+getStdin()
+.then(function (str) {
+  if (str) {
+    console.log(ducksay(trim.right(str)))
+  } else {
+    var argv = require('yargs').argv
+    if (argv._.length > 0) {
+      console.log(ducksay(argv._.join(' ')))
+    } else {
+      console.log(ducksay('...'))
+    }
+  }
+})
